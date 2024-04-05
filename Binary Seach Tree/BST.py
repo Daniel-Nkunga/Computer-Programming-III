45115a99ccdee3107e3d2e393c4d6bd15949a30f
Daniel-Nkunga <144631064+Daniel-Nkunga@users.noreply.github.com>
2024-04-05 00:05:38 -0500

    Delete function implemented
    Sets the right not was the main note if a node has two children

--- Binary Seach Tree/BST.py
+++ Binary Seach Tree/BST.py
@@ -51,5 +51,49 @@ class TreeNode:
                 return self.right.find(value)
         else:
             return True
+        
+    def get_min_value_node(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+    
+    def delete_node(self, value):
+        if self is None:
+            return self
+        
+        if value < self.value:
+            self.left = self.left.delete_node(value)
+        elif value > self.value:
+            self.right = self.right.delete_node(value)
+        else:
+            if self.left is None:
+                temp = self.right
+                self = None
+                return temp #Deletes node
+            elif self.right is None:
+                temp = self.left
+                self = None
+                return temp #Deletes node
+            
+            #For nodes with two children
+            temp = self.get_min_value_node(self.right) #Shift tree from right children
+            self.value = temp.value
+            self.right = self.right.delete_node(temp.value)
+
+        return self
+        
+tree = TreeNode(8)
+tree.insert(16)
+tree.insert(4)
+tree.insert(15)
+tree.insert(17)
+tree.insert(5)
+tree.insert(3)
+
+tree.preorder_traversal()
+print()
+tree.delete_node(5)
+tree.preorder_traversal()
 
 ##This is just so I have something to commit
