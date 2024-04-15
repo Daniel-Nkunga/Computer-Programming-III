98e88ec4b9075082672fec100d0b5fc68a88c128
Daniel-Nkunga <144631064+Daniel-Nkunga@users.noreply.github.com>
2024-04-14 19:58:08 -0500

    HashTable now handles collisions
    Thank you BeAPythonDev: https://www.youtube.com/watch?v=fdfF8AnkG6s

--- HashTables/hashtables.py
+++ HashTables/hashtables.py
@@ -1,7 +1,7 @@
 import hashlib
 
 class HashTable(object):
-    def __init__(self, size = 10): #Size should porblaby need to be removed
+    def __init__(self, size = 3): #Size should porblaby need to be removed
         self.num_elements = 0 #Number of elements in the table
         self.data = [0] * size
         self.size = len(self.data) #Allows for incrimental resizing
@@ -13,40 +13,63 @@ class HashTable(object):
         return our_hash % self.size
         
     def __resize(self):
-        pass
+        raise NotImplementedError
 
     def insert(self, key, value):
         hash_data = (key, value)
         hash_index = self.__get_hash_index(key)
-        self.data[hash_index] = hash_data
+        
+        if self.data[hash_index] == 0:
+            self.data[hash_index] = [hash_data]
+        else:
+            self.data[hash_index].append(hash_data)
         self.num_elements += 1
 
-    def get(self, key):
+    def __get_data_index_tuple(self, key):
         hash_index = self.__get_hash_index(key)
-        data = self.data[hash_index]
-        dk = data[0]
-        if key != dk or data == 0:
-            raise KeyError("Hash key does not exist")
-        return data[1]
-        
+        data_list = self.data[hash_index]
+        if data_list == 0:
+            return None
+        for data_index, data in enumerate(data_list):
+            dk = data[0]
+            if dk == key:
+                return (hash_index, data_index)
         
 
+    def get(self, key):
+        dit = self.__get_data_index_tuple(key)
+        if dit:
+            return self.data[dit[0]][dit[1]][1]
+        raise KeyError("Hash key does not exist")
+
     def remove(self, key):
-        hash_index = self.__get_hash_index(key)
-        data = self.data[hash_index]
-        dk = data[0]
-        if key != dk or data == 0:
+        dit = self.__get_data_index_tuple(key)
+        if not dit:
             raise KeyError("Hash key does not exist")
-        self.data[hash_index] = 0
+        if len(self.data[dit[0]]) == 1:
+            self.data[dit[0]] = 0
+        else:
+            del self.data[dit[0]][dit[1]]
         self.num_elements -= 1
 
     def key_contains(self, substring): #Extra thing he is doing that 
-        pass        
+        raise NotImplementedError        
 
 
-hash = HashTable()
-hash.insert( 0, 3.14159)
+our_hash_table = HashTable()
+names = ["Jim", "Mark","Steve", "Tony", "Jimbo", "Jimbob", "Havana", "Rashesh"]
+name_tuples = [(n,f"{n}@gmail.com") for n in names]
+ 
+for name_data in name_tuples:
+    our_hash_table.insert(name_data[0], name_data)
 print("== Insert data ==")
-print(hash.data)
-print(hash.num_elements)
-print(hash.get(0))
+print(our_hash_table.data)
+print(our_hash_table.num_elements)
+print("==Get data==")
+print(our_hash_table.get("Jim"))
+print("== Remove data==")
+our_hash_table.remove("Jim")
+print(our_hash_table.data)
+our_hash_table.remove("Mark")
+print(our_hash_table.data)
+print(our_hash_table.num_elements)
